c9691cb832feca97c9af2743150efd62a3086584
Glitch (time-scanner) <none>
2018-01-06 12:54:49 +0000

    added the view engine

--- server.js
+++ server.js
@@ -3,9 +3,10 @@
 
 // init project
 var express = require('express');
-var exphbs     = require('express-handlebars');
+var exphbs  = require('express-handlebars');
 var path    = require('path');
-var app = express();
+
+var app     = express();
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -15,14 +16,9 @@ app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'})); 
 app.set('view engine', 'handlebars');
-var options = { dotfiles: 'ignore', etag: false,
-extensions: ['htm', 'html'],
-index: false
-};
-app.use(express.static(path.join(__dirname, 'public') , options  ));
 app.get('/', function(req, res)
 {
-res.render('hello');   // this is the important part
+res.render('index');   // this is the important part
 });
 
 // listen for requests :)
