ba420f54a2f90b80693ba23b5e5b0d7959e3d6fe
Glitch (time-scanner) <none>
2018-01-06 12:45:11 +0000

    💳🚅 Checkpoint views/index.html:415617/98 server.js:415617/94

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 // init project
 var express = require('express');
-var hbs     = require('handlebars');
+var exphbs     = require('express-handlebars');
 var path    = require('path');
 var app = express();
 
@@ -13,7 +13,17 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// 
+app.engine('handlebars', exphbs({defaultLayout: 'main'})); 
+app.set('view engine', 'handlebars');
+var options = { dotfiles: 'ignore', etag: false,
+extensions: ['htm', 'html'],
+index: false
+};
+app.use(express.static(path.join(__dirname, 'public') , options  ));
+app.get('/', function(req, res)
+{
+res.render('hello');   // this is the important part
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
