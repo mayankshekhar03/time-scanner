738d1ccc9bc08d1988f147f4db5d0302654ab1a4
Glitch (time-scanner) <none>
2018-01-06 12:34:56 +0000

    🎯👯 Checkpoint server.js:415617/131 views/index.html:415617/37 views/layout/main.handlebars:415617/1

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 
 // init project
 var express = require('express');
+var hbs     = require('handlebars');
+var path    = require('path');
 var app = express();
 
 // we've started you off with Express, 
@@ -11,27 +13,7 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+// 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
